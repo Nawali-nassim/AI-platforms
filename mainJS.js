e51ce2e84b816eeb3989b7b6fa690e4dd9b8522c
Nawal <nawalahmadstayteh@gmail.com>
2025-09-02 21:27:31 +0300

    eigthth update on 2/9/2025

--- mainJS.js
+++ mainJS.js
@@ -9,4 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+    const menuToggle = document.querySelector('.menu-toggle');
+        const menu = document.querySelector('nav ul');
+        menuToggle.addEventListener('click', () => {
+            menu.classList.toggle('active');
+        });
 });
+        
